629f8b7c0ffe8ce7425e213bbf437460be325ed5
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-03-16 16:47:53 -0300

    Cadastro Simples de Clientes
    Cadastro Simples de Clientes

---
+++ CadCli/CadCli/Controllers/ClientesController.cs
@@ -0,0 +1,43 @@
+﻿using CadCli.Data;
+using CadCli.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadCli.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private CadCliDataContext _ctx = new CadCliDataContext();
+
+        public ViewResult Index()
+        {
+            //var clientes = new List<Cliente>()
+            //{
+            //    new Cliente() { Id = 1, Nome = "Fabiano", Idade = 39},
+            //    new Cliente() { Id = 2, Nome = "Priscila", Idade = 40},
+            //    new Cliente() { Id = 3, Nome = "Raphael", Idade = 19},
+            //    new Cliente() { Id = 4, Nome = "Isabel", Idade = 61},
+            //    new Cliente() { Id = 5, Nome = "Nair", Idade = 70}
+            //};
+
+            var clientes = _ctx.Clientes.ToList();
+            return View(clientes);
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(Cliente cli)
+        {
+            _ctx.Clientes.Add(cli);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
